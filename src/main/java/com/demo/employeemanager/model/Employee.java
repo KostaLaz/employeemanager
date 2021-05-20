e37f9cedcb274a24cef96fbb88b4da5a5c9c0803
KostaLaz <kosta.lazarevski89@gmail.com>
2021-05-20 14:13:56 +0200

    Employee model

--- src/main/java/com/demo/employeemanager/model/Employee.java
+++ src/main/java/com/demo/employeemanager/model/Employee.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Data
@@ -18,12 +15,14 @@ public class Employee implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
     private long id;
     private String name;
     private String email;
     private String jobTitle;
     private String phone;
     private String image;
+    @Column(nullable = false, updatable = false)
     private String employeeCode;
 
 }
