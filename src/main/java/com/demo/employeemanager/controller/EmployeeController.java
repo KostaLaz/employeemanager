cf8aed72084e7cfc32448140124c10b13d13b4c8
KostaLaz <kosta.lazarevski89@gmail.com>
2021-05-20 16:08:49 +0200

    employee controller

--- src/main/java/com/demo/employeemanager/controller/EmployeeController.java
+++ src/main/java/com/demo/employeemanager/controller/EmployeeController.java
@@ -7,11 +7,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.print.attribute.standard.ReferenceUriSchemesSupported;
 import java.util.List;
 
 @RestController
