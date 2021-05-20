5f9d23bb77ab4bc37f9946282aa4e283d397e791
KostaLaz <kosta.lazarevski89@gmail.com>
2021-05-20 15:39:38 +0200

    employee service - add employee logic

---
+++ src/main/java/com/demo/employeemanager/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.demo.employeemanager.service;
+
+import com.demo.employeemanager.model.Employee;
+import com.demo.employeemanager.repository.EmployeeRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeService {
+    private EmployeeRepository employeeRepository;
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepository.save(employee);
+    }
+}
