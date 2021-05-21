0433d0d1ca6d914fc316a1102335344ef6103e2d
KostaLaz <kosta.lazarevski89@gmail.com>
2021-05-21 17:26:53 +0200

    employee crud

--- src/main/java/com/demo/employeemanager/repository/EmployeeRepository.java
+++ src/main/java/com/demo/employeemanager/repository/EmployeeRepository.java
@@ -4,10 +4,12 @@ import com.demo.employeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    @Transactional
     void deleteEmployeeById(long id);
 
     Optional<Employee> findEmployeeById(long id);
